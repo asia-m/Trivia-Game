0a438cb4c8b7c14a541437271d36247d29f3f365
Asia M <asiam@reddies-MacBook-Pro.local>
2019-07-27 15:41:30 -0400

    Uploading homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,63 @@
 
-setTimeout(fiveSeconds, 1000 * 5);
-setTimeout(tenSeconds, 1000 * 10);
-setTimeout(timeUp, 1000 * 15);
 
-function fiveSeconds() {
-  
-  $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
-  console.log("10 seconds left");
-}
+setTimeout(timeUp, 1000 * 30);
 
-function tenSeconds() {
-
-  $("#time-left").append("<h2>About 5 Seconds Left!</h2>");
-  console.log("5 seconds left");
-}
 
 function timeUp() {
   console.log("done");
   $("#time-left").append("<h2>Time's Up!</h2>");
   console.log("time is up");
+  //When time is up alert should pop up with results and a messsage//
+  questions.forEach(function (question, i) {
+    // get access to a radio button for current question
+    var userResponse = $(`input[name='input-${i}']:checked`).val();
 
-  audio.play();
+    // compare the userResponse to the question.a
+    if (userResponse === question.a) {
+      score++;
+    }
+
+    console.log(userResponse);
+  });
+  updateScore();
 }
+
+var questions = [
+  { q: "Priness Jasmine is a character in Aladdin", a: "t" },
+  { q: "Simba is played by Jason Weaver in the original Lion King", a: "t" },
+  { q: "Snow White is a priness in Snow White and The 6 Dwarves", a: "f" },
+  { q: "Disney was created and branded by Paul Disney", a: "f" },
+  { q: "Mickey Mouse's original debut year was 1995", a: "f" }
+];
+
+
+var score = 0;
+
+var questionIndex = 0;
+
+
+function renderQuestion() {
+  questions.forEach(function (question, i) {
+    $("#question").append(
+      "<p>" + question.q + "</p>" +
+      `<input type="radio" name= "input-${i}" value="t">True<br>
+      <input type="radio" name="input-${i}" value="f">False <br>`
+    )
+
+  })
+
+}
+
+function updateScore() {
+  alert("Time's Up!\n\n Your Score: " + score)
+  document.querySelector("#score").innerHTML = "Score: " + score;
+}
+
+
+renderQuestion();
+
+
+
+
+
+
